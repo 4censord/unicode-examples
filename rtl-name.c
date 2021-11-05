490f967a215b4b860f64545c534fc0a8c3d9853f
4censord <mail@business-insulting.de>
2021-11-05 10:26:00 +0100

    Added rtl-name example
    This includes a comment with a name that's normally written rtl

---
+++ rtl-name.c
@@ -0,0 +1,18 @@
+/* Copyright (C) 2021 ويكيبيديا‏
+ * The name should be "wikipedia" transliterated to arabic
+ * 
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ */
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+    bool isAdmin = false;
+    if (isAdmin) {
+        printf("You are an admin.\n");
+    } else {
+        printf("You not an admin.\n");
+    }
+    return 0;
+}
