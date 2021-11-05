5c15ff55daafaf9faced7810ab172078062b25ec
4censord <mail@business-insulting.de>
2021-11-05 10:59:14 +0100

    This adds a programm with some comment containing rtl text

---
+++ function_comment.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// This funtion has been written by ويكيبيديا‏
+// The name should be "wikipedia" transliterated to arabic
+// This is for reference only
+func main() {
+	var isAdmin = false
+	var isSuperAdmin = false
+	isAdmin = isAdmin || isSuperAdmin
+
+	// go ask ويكيبيديا‏ if this breaks
+	if isAdmin {
+		fmt.Println("You are an admin.")
+	}
+}
